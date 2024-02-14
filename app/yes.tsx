cf841f6592f9ebba1f917c90109c078de2a0665a
SoorajModi <soomodi@gmail.com>
2024-02-13 22:37:04 -0500

    Fix apostrophe

--- app/yes.tsx
+++ app/yes.tsx
@@ -4,7 +4,7 @@ export default function Yes() {
     return (
         <>
             <Image src="/corgi-yes.gif" alt="Another corgi gif" height={300} width={300} priority />
-            <p className="text-black text-4xl">Ok, yay! I'll see you soon!!</p>
+            <p className="text-black text-4xl">Ok, yay! I&apos;ll see you soon!!</p>
         </>
     )
 }
