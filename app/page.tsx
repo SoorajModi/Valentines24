9a952d8e94d4f374a0f29f606ef28547ab219dea
SoorajModi <soomodi@gmail.com>
2024-02-13 22:31:11 -0500

    Add button functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,45 @@
+"use client";
+import { useState } from 'react';
 import Image from "next/image";
+import Yes from './yes';
 
 export default function Home() {
-
+  const [yes, setYes] = useState(false)
+  const [no, setNo] = useState(false)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col space-y-8 text-center items-center justify-center">
-        <Image src="/corgi-love.gif" alt="Corgi love gif" height={300} width={300} priority />
-        <p className="text-black text-4xl">Will you be my Valentine?</p>
-        <div className="flex flex-row items-center justify-center space-x-4">
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-16">Yes</button>
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-16">No</button>
-        </div>
+        {
+          yes ? <Yes /> : 
+          (
+            <>
+              {
+                no ? (
+                  <>
+                    <Image src="/corgi-no.jpeg" alt="Corgi sad" height={300} width={300} priority />
+                    <p className="text-black text-4xl">Haha, really funny, try again...</p>
+                  </>
+                ) 
+              :
+                (
+                  <>
+                    <Image src="/corgi-love.gif" alt="Corgi love gif" height={300} width={300} priority />
+                    <p className="text-black text-4xl">Will you be my Valentine?</p>
+                  </>
+                )
+              }
+              <div className="flex flex-row items-center justify-center space-x-4">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-16" onClick={() => setYes(true)}>
+                  Yes
+                </button>
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-16" onClick={() => setNo(true)}>
+                  No
+                </button>
+              </div>
+            </>
+          )
+        }
       </div>
     </main>
   );
---
+++ app/yes.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Yes() {
+    return (
+        <>
+            <Image src="/corgi-yes.gif" alt="Another corgi gif" height={300} width={300} priority />
+            <p className="text-black text-4xl">Ok, yay! I'll see you soon!!</p>
+        </>
+    )
+}
